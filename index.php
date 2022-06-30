ec3758b8ddb9eba35980cb0468c781e0da86ad62
L00161862 <74217716+L00161862@users.noreply.github.com>
2022-06-30 15:59:21 +0100

    Junior Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Reverse a string using strrev()
+  
+function Reverse($str){
+    return strrev($str);
+}
+
+$str = " .emordnilaP " . " a " . " si " . " RacecaR ";
+echo Reverse($str)
+?>
